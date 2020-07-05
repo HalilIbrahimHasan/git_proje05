85984688d0b17d4399a39b2f6cf69f61204937fa
SelimTopbas <67221339+SelimTopbas@users.noreply.github.com>
2020-07-05 22:14:35 +0200

    Update ClassYeni.java
    deneme yapıyorum.Selim Topbas

--- src/yenipackage/ClassYeni.java
+++ src/yenipackage/ClassYeni.java
@@ -5,5 +5,6 @@ public class ClassYeni {
 	
 	public static void main(String[] args) {
 		System.out.println("ilk commit masterdan");
+		System.out.println("Merhabalar hocam. deneme yapıyorum");
 	}
 }
